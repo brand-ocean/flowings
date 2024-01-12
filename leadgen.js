2103f2ea0b42769f27b7098ac0933d7d33cbea2d
Arin Issa <info@brandocean.nl>
2024-01-12 01:08:01 +0100

    Update leadgen.js

--- leadgen.js
+++ leadgen.js
@@ -7,17 +7,6 @@ Webflow.push(function () {
 //Loading flatpickr JS
 	flatpickr(".date");
 
-.flatpickr-mobile:before  {
-		content: attr(placeholder);
-		color: gray;
-	  }
-	  .flatpickr-mobile:focus[value]:not([value=""]):before {
-		display: none;
-	  }
-	  
-	  input[type="hidden"][value]:not([value=""]) + .flatpickr-mobile:before {
-		display: none; 
-	  }
 
 
 
